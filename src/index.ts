f7578c42d454df5960a4e559b186b2ecb0713a8b
carlos <carlosfebres97@hotmail.com>
2021-07-23 23:30:57 -0400

    refactor: export getFactoryContractAddress and getModuleContractAddress

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export {
   calculateProxyAddress,
   getFactoryAndMasterCopy,
   getModuleInstance,
+  getModuleContractAddress,
+  getFactoryContractAddress,
 } from "./factory";
 export { KnownContracts, ContractAddresses, KnownModules } from "./types";
 export { CONTRACT_ADDRESSES, CONTRACT_ABIS } from "./constants";
